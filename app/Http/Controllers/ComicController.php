0e21bcfd31fa2a389dbd78e1b102b6f81c36dd6b
giovannilampis <giolampis@hotmail.com>
2023-06-14 16:28:23 +0200

    impemented index method in ComicController.php

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -14,7 +14,7 @@ class ComicController extends Controller
      */
     public function index()
     {
-        return 'index';
+        return response()->view('comics.index', []);
     }
 
     /**
