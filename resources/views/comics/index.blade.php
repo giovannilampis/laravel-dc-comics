a211b45e3691c450cdb839be161e9509f361ecde
giovannilampis <giolampis@hotmail.com>
2023-06-16 17:59:58 +0200

    push

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,6 +3,12 @@
 
 <div class="container">
 
+  @if(Session::has('success'))
+    <div class="alert alert-success">
+      { !! Session::get('success') !! }
+    </div>
+  @endif
+
     <div class="row flex-wrap">
 
         @foreach($rows as $row)
