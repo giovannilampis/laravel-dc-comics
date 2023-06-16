81729c98668d0f2fce0bf0ef12450e83c77a76c1
giovannilampis <giolampis@hotmail.com>
2023-06-16 15:36:49 +0200

    add required field message error in create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -2,6 +2,16 @@
 @extends('layouts.app')
 @section('content')
 
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 <div class="container">
     <p class="text-center fs-2 my-5 text-uppercase">add a comic book to this collection</p>
     <form action="{{ route('comics.store') }}" method="POST">
@@ -13,7 +23,6 @@
         <div class="mb-3">
             <label class="form-label">Description</label>
             <textarea name="description" id="" style="width: 100%"></textarea>
-            {{-- <input name="description" type="text" class="form-control"> --}}
         </div>
         <div class="mb-3">
             <label class="form-label">Thumb</label>
