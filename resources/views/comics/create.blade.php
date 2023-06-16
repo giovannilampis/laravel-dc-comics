948b34b1677868f89129178956f0e5cdb6c4fb61
giovannilampis <giolampis@hotmail.com>
2023-06-16 15:52:12 +0200

    add @error Directive for error while writing Title

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -18,7 +18,10 @@
         @csrf
         <div class="mb-3">
           <label class="form-label">Title</label>
-          <input name="title" type="text" class="form-control">
+          <input name="title" type="text" class="form-control @error('title') is-invalid @enderror">
+          @error('title')
+          <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
         </div>
         <div class="mb-3">
             <label class="form-label">Description</label>
