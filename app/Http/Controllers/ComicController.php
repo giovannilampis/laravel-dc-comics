b8507c3d1aed6c1b86859bf5c4ddc12c7dd25c0f
giovannilampis <giolampis@hotmail.com>
2023-06-16 16:05:19 +0200

    add more possible errors for Title field

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -39,10 +39,12 @@ class ComicController extends Controller
     {
         $request->validate(
             [
-            'title' => 'required'
+            'title' => 'required|max:20|unique:comics'
             ],
             [
-            'title.required' => 'Il campo "Title" deve essere necessariamente riempito'
+            'title.required' => 'Il campo "Title" deve essere necessariamente riempito',
+            'title.max' => 'Bisogna scegliere un titolo composto da non più di 20 caratteri',
+            'title.unique' => "Non può essere scelto un titolo già assegnato ad un'altra rivista"
             ]
     );
 
