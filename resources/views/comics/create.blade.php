12c21c31fd5a3c514715f76299a8c2085f91ad6a
giovannilampis <giolampis@hotmail.com>
2023-06-16 13:03:45 +0200

    change create.blade.php layout

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -2,39 +2,42 @@
 @extends('layouts.app')
 @section('content')
 
-<form action="{{ route('comics.store') }}" method="POST">
-    @csrf
-    <div class="mb-3">
-      <label class="form-label">Title</label>
-      <input name="title" type="text" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Description</label>
-        <input name="description" type="text" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Thumb</label>
-        <input name="thumb" type="text" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Price</label>
-        <input name="price" type="text" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label class="form-label">series</label>
-        <input name="series" type="text" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Sale Date</label>
-        <input name="sale_date" type="text" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Type</label>
-        <input name="type" type="text" class="form-control">
-    </div>
-
-    <button type="submit" class="btn btn-primary">Submit</button>
-
-  </form>
+<div class="container">
+    <p class="text-center fs-2 my-5 text-uppercase">add a comic book to this collection</p>
+    <form action="{{ route('comics.store') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+          <label class="form-label">Title</label>
+          <input name="title" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Description</label>
+            <input name="description" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Thumb</label>
+            <input name="thumb" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Price</label>
+            <input name="price" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">series</label>
+            <input name="series" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Sale Date</label>
+            <input name="sale_date" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Type</label>
+            <input name="type" type="text" class="form-control">
+        </div>
+    
+        <button type="submit" class="btn btn-primary">CREATE</button>
+    
+      </form>
+</div>
 
   @endsection
